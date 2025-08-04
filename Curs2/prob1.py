e419ccbc175c9bf349027d8bcdcc470371835173
Mihai Flavius <am_ban_pe_github@yahoo.com>
2025-08-04 18:05:05 +0300

    Delete Curs2 directory

--- Curs2/prob1.py
+++
@@ -1,8 +0,0 @@
-# verificam daca un numar este divizibil cu 5
-numar_verificare=52
-
-if numar_verificare % 5 == 0:
-    print(f"Variabila {numar_verificare} este divizibila cu 5")
-else:
-    print(f"Variabila {numar_verificare} nu este divizibila cu 5")
-    
