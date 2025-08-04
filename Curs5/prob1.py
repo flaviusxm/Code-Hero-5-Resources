f206c67a0ee4e521df7f5eb88b448a27102d096f
Mihai Flavius <am_ban_pe_github@yahoo.com>
2025-08-04 18:05:34 +0300

    Delete Curs5 directory

--- Curs5/prob1.py
+++
@@ -1,8 +0,0 @@
-latime=int(input("Va rog introduceti o latime pt a afisa # : "))
-
-if latime>0 :
-    print(f"Caracterul # se va afisa de {latime} ori")
-    print(f"@"*latime)
-else:
-    print("Mai incearca")
-    
